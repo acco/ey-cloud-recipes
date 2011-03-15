0539e64a6e2ea45010814ed3d785e5290617d33f
Danish Khan <danishkhan@danishkhan.org>
2011-03-15 13:33:33 -0700

    Updating gollum script error of serve

--- cookbooks/gollum/recipes/default.rb
+++ cookbooks/gollum/recipes/default.rb
@@ -4,6 +4,7 @@
 #
 execute "stop nginx" do
   command "/etc/init.d/nginx stop"
+  only_if "/etc/init.d/nginx status"
 end
 
 execute "generate static files" do
@@ -11,5 +12,5 @@ execute "generate static files" do
 end
 
 execute "start gollum server" do
-  command "cd /data/ey_wiki/current/ && sudo gollum-site server --port 80"
+  command "cd /data/ey_wiki/current/ && sudo gollum-site serve --port 80"
 end  
