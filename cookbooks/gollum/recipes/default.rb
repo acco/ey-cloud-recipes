c0185a7d18313081ed68d36a09895b55634971de
Danish Khan <danishkhan@danishkhan.org>
2011-03-15 14:06:09 -0700

    updated gollum recipe

--- cookbooks/gollum/recipes/default.rb
+++ cookbooks/gollum/recipes/default.rb
@@ -6,11 +6,3 @@ execute "stop nginx" do
   command "/etc/init.d/nginx stop"
   only_if "/etc/init.d/nginx status"
 end
-
-execute "generate static files" do
-  command "cd /data/ey_wiki/current/ && gollum-site generate"
-end
-
-execute "start gollum server" do
-  command "cd /data/ey_wiki/current/ && sudo gollum-site serve --port 80 &"
-end  
